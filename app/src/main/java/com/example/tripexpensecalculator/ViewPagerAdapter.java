2d1fdf074791c0a887e6fb8d7c3db622ef58b73d
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-03 23:37:31 +0530

    Update ViewPagerAdapter.java

--- app/src/main/java/com/example/tripexpensecalculator/ViewPagerAdapter.java
+++ app/src/main/java/com/example/tripexpensecalculator/ViewPagerAdapter.java
@@ -1,6 +1,19 @@
+package com.example.tripexpensecalculator;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.tripexpensecalculator.fragments.ExpenseFragment;
+import com.example.tripexpensecalculator.fragments.FriendsFragment;
+import com.example.tripexpensecalculator.fragments.ReportFragment;
+import com.example.tripexpensecalculator.fragments.SummaryFragment;
+
 public class ViewPagerAdapter extends FragmentStateAdapter {
-    public ViewPagerAdapter(@NonNull FragmentActivity fa) {
-        super(fa);
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
     }
 
     @NonNull
