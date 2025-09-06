9fe0acdb6409969670491c0a7ff6d7b998521c6b
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-06 15:48:29 +0530

    Update FriendsFragment.java

--- app/src/main/java/com/example/tripexpensecalculator/fragments/FriendsFragment.java
+++ app/src/main/java/com/example/tripexpensecalculator/fragments/FriendsFragment.java
@@ -212,7 +212,6 @@ public class FriendsFragment extends Fragment {
                 cardBox.addView(botInner);
 
                 friendsListLayout.addView(cardBox);
-                friendCount++;
             }
 
             // Place ADD FRIEND and DELETE FRIEND buttons after the list
