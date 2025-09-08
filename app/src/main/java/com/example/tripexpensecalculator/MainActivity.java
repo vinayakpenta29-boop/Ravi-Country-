e3cda63b59037306972a92c19c2a05587a833eb7
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-08 21:06:15 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/tripexpensecalculator/MainActivity.java
+++ app/src/main/java/com/example/tripexpensecalculator/MainActivity.java
@@ -1,6 +1,8 @@
-package com.example.tripexpensecalculator;
+ppackage com.example.tripexpensecalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,7 +24,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // ----- Make activity fullscreen (hide status bar) -----
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        // ------------------------------------------------------
+
         setContentView(R.layout.activity_main);
+
+        // Optional: "sticky immersive" mode, uncomment if you want to hide navigation too
+        /*
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+        */
 
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPager);
