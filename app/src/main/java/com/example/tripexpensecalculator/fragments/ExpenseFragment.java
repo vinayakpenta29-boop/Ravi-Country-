e11b61cadb72a8bd2fa206f6da9219805eb16a5c
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-09 23:45:14 +0530

    Update ExpenseFragment.java

--- app/src/main/java/com/example/tripexpensecalculator/fragments/ExpenseFragment.java
+++ app/src/main/java/com/example/tripexpensecalculator/fragments/ExpenseFragment.java
@@ -67,7 +67,7 @@ public class ExpenseFragment extends Fragment {
         btnDeleteExpense = new Button(getContext());
         btnDeleteExpense.setText("DELETE A EXPENSE");
         btnDeleteExpense.setAllCaps(true);
-        btnDeleteExpense.setTextColor(getResources().getColor(android.R.color.navyblue));
+        btnDeleteExpense.setTextColor(getResources().getColor(android.R.color.navy_blue));
         btnDeleteExpense.setTextSize(18);
         btnDeleteExpense.setTypeface(android.graphics.Typeface.DEFAULT_BOLD);
         btnDeleteExpense.setBackgroundResource(R.drawable.curved_gray_button);
