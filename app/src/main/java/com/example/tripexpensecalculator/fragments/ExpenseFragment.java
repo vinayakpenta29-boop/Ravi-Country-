858d4e32bdf2fd777edd9ab9e072f92418a06b7c
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-12-08 11:57:15 +0530

    Update ExpenseFragment.java

--- app/src/main/java/com/example/tripexpensecalculator/fragments/ExpenseFragment.java
+++ app/src/main/java/com/example/tripexpensecalculator/fragments/ExpenseFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
